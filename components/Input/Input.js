8d11a88a08a9dd69d8cc078992c4903ded522892
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2022-07-25 18:42:04 +0300

    Initial commit

---
+++ components/Input/Input.js
@@ -0,0 +1,11 @@
+import classes from './Input.module.css';
+
+const Input = () => {
+    return (
+        <div className={classes.container}>
+            Input
+        </div>
+    )
+}
+
+export default Input
---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const rd_mul = (a, b) => {
+    return a * b;
+}
+
+module.exports = rd_mul;
